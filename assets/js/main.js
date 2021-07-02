56a607dca5e5393b698e8b904ceda91601028932
cristian <calveal2004@gmail.com>
2021-07-01 23:58:22 -0400

    cambios estilos y Jquery

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+ 
+    // Add smooth scrolling to all links
+    $(".nav-link").on('click', function(event) {
+  
+       event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html').animate({
+          scrollTop: $(hash).offset().top - 5,
+        }, 1000
+        
+           );
+      
+    });
+   
+  });
+
+  $('[data-toggle="tooltip"]').tooltip()
+
+  $('[data-toggle="popover"]').popover()
